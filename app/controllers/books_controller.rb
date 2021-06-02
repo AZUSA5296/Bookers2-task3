05df7b2cce47f7c6265f9433778ba90b9d1a7075
AZUSA5296 <nnfnnfnakuyo731@gmail.com>
2021-06-02 06:08:05 +0000

    [finish]task_naster_2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,16 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:update, :edit]
 
   def show
+    @newbook = Book.new
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def create
@@ -14,6 +19,7 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
+      @user = current_user
       @books = Book.all
       render 'index'
     end
@@ -34,16 +40,23 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+     unless @book.user == current_user
+     redirect_to books_path
+   end
   end
 
 end
